9c05861ef1c4ef07b2cffa8f10c226d96f8b126b
Faiz Ahmad <faizahmd931@gmail.com>
2025-03-02 01:01:44 -0800

    changed resume viewer

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const NextConfig = {
     return [
       {
         source: '/portfolio',
-        destination: '/static-site/portfolio/index.html',
+        destination: '/site/portfolio/index.html',
       },
       {
         source: '/web-video-player',
-        destination: '/static-site/web-video-player/index.html',
+        destination: '/site/web-video-player/index.html',
       },
       // Add more static site rewrites as needed
     ]
